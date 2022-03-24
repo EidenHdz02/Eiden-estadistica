c242f3099d0a32bde3364ce0d15ffb82ebf190c1
Eiden Noe Martinez Hernandez <Eidennoe100MartinezHernandez@hotmail.com>
2022-03-24 09:16:14 -0600

    update

---
+++ clases/examen.R
@@ -0,0 +1,23 @@
+# examen 
+#hipotesis nula 
+#hipotesis alternativa 
+
+# ejercicio 1
+peso neto<- (80$kg)
+contenido neto<- (44$costales)
+
+t.test( = T)
+
+# procedimiento general para la prueba de t una muestra 
+costal<- c(87.7, 80.01, 77.28, 78.76, 81.52, 74.2, 80.71, 79.5, 77.87, 81.94, 80.7, 82.32, 75.78, 80.19, 83.91, 79.4, 77.52, 77.62, 81.4, 74.89, 82.95
+           73.59, 77.92, 77.18, 79.83, 81.23, 79.28, 78.44, 79.01, 80.47, 76.23, 78.89, 77.14, 69.94, 78.54, 79.7, 82.45, 77.29, 75.52, 77.21, 75.99, 81.94, 80.41, 77.7)
+mean(costal)
+
+#ejercicio 2
+
+# cantidad maxima de toneladas por año emitida no debe superar las 17.5 Ton/año 
+azufre<- c(15.8, 22.7, 26.8, 19.1, 18.5, 14.4 8.3, 25.9, 26.4, 9.8, 22.7, 15.2, 23.0, 29.6, 21.9, 10.5, 17.3, 6.2, 18.0, 22.9
+           24.6, 19.4, 12.3, 15.9, 11.2, 14.7, 20.5, 26.6, 20.1, 17.0, 23.3, 27.5, 23.9, 17.5, 11.0, 20.4, 16.2, 20.8, 13.3, 18.1)
+
+emisiones de oxido de azufre <- C(17.Ton/ha)
+
